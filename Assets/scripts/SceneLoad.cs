bac1446d1ece9e46d84e2944ba1372a711db5f34
Llarence <hillo>
2018-05-05 00:24:21 -0600

    stuff

--- Assets/scripts/SceneLoad.cs
+++ Assets/scripts/SceneLoad.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class SceneLoad : MonoBehaviour {
 
+	public string sP;
+	public VideoPlayer V;
+	public VideoClip vC;
 
 	// Use this for initialization
 	public void LoadScene (string s) {
-		SceneManager.LoadScene (s);
+		sP = s;
+		StartCoroutine (w ());
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	IEnumerator w () {
+		if (sP == "example") {
+			V.clip = vC;
+			yield return new WaitForSeconds (13);
+		}
+		SceneManager.LoadScene (sP);
 	}
 }
